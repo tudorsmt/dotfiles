0ff8734d0323c99e13764907a5c574beb07882a2
Tudor Tabacel <tudorsmt@gmail.com>
2018-03-23 14:46:57 +0100

    Adapted setup for desktop machine with 2 displays, no gnome
    - Script for detecting display can set up 2 or 3 displays
    - fixed gnome keyring startup for OS installations that are not based on
    gnome 3

--- i3/scripts/setup_displays.sh
+++ i3/scripts/setup_displays.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$(xrandr | grep " connected" -c)" = "3" ]; then
-    echo "3 displays detected, deplying settings"
-    ~/.screenlayout/in-dock.sh
-else
-    echo "Haven't detected 3 displays, doing nothing"
-fi
+DISPLAY_NUMBER=$(xrandr | grep " connected" -c)
+
+case "${DISPLAY_NUMBER}" in
+    "3")
+        echo "3 displays detected, deploying settings"
+        ~/.screenlayout/in-dock.sh
+        ;;
+    "2")
+        echo "2 displays detected, deploying settings"
+        ~/.screenlayout/dual-display-desktop.sh
+        ;;
+    *)
+        echo "Detected ${DISPLAY_NUMBER}, doing nothing"
+        ;;
+esac
