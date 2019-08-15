88ced3ef29e3c47b6273d35a4973b747d70a2579
Tudor Tabacel <tudorsmt@gmail.com>
2019-08-15 09:08:59 +0200

    desktop screen layout: fixed outputs post nvidia driver install

--- .screenlayout/dual-display-desktop.sh
+++ .screenlayout/dual-display-desktop.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-xrandr --output DVI-I-1 --off --output DP-2 --mode 1920x1200 --pos 1920x0 \
-       --rotate right --output DP-1 --primary --mode 1920x1200 --pos 0x440 --rotate normal
+xrandr --output DVI-I-1 --off \
+       --output DVI-I-0 --off \
+       --output DP-3 --primary --mode 1920x1200 --pos 0x430 --rotate normal \
+       --output DP-2 --mode 1920x1200 --pos 1920x0 --rotate right \
+       --output DP-1 --off --output DP-0 --off
