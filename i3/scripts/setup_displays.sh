ecf821d49a113af8a39659d5696ea4c01a187f58
Tudor Tabacel <tudorsmt@gmail.com>
2020-11-20 09:35:44 +0100

    setup displays: autodetect from host and # of displays

--- i3/scripts/setup_displays.sh
+++ i3/scripts/setup_displays.sh
@@ -2,18 +2,28 @@
 
 DISPLAY_NUMBER=$(xrandr | grep " connected" -c)
 
-case "${DISPLAY_NUMBER}" in
-    "3")
-        echo "3 displays detected, deploying settings in 5 seconds"
-        sleep 5
-        ~/.screenlayout/in-dock.sh
-        ;;
-    "2")
-        echo "2 displays detected, deploying settings in 5 seconds"
-        sleep 5
-        ~/.screenlayout/dual-display-desktop.sh
-        ;;
-    *)
-        echo "Detected ${DISPLAY_NUMBER}, doing nothing"
-        ;;
-esac
+host=$(hostname)
+
+host_script="${HOME}/.screenlayout/${host}-${DISPLAY_NUMBER}.sh"
+
+if [ -e "${host_script}" ]; then
+    echo "Executing ${host_script} in 5s"
+    sleep 5
+    "${host_script}" && exit 0
+else
+    case "${DISPLAY_NUMBER}" in
+        "3")
+            echo "3 displays detected, deploying settings in 5 seconds"
+            sleep 5
+            ~/.screenlayout/in-dock.sh
+            ;;
+        "2")
+            echo "2 displays detected, deploying settings in 5 seconds"
+            sleep 5
+            ~/.screenlayout/dual-display-desktop.sh
+            ;;
+        *)
+            echo "Detected ${DISPLAY_NUMBER}, doing nothing"
+            ;;
+    esac
+fi
