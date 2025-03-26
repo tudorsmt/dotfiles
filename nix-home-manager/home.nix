b1723895ee41efb265b052263e848f7bc87cf1da
Tudor Tabacel <tudorsmt@gmail.com>
2025-03-26 16:33:30 +0100

    home-manager: add UV

--- nix-home-manager/home.nix
+++ nix-home-manager/home.nix
@@ -38,6 +38,7 @@
     pkgs.superfile
     pkgs.sysstat
     pkgs.tig
+    pkgs.uv
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
